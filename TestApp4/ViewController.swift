7611badaa6bc56cd5e1ada74f28c12b43ebce275
je-kim-ktec <je-kim@kddi-tech.com>
2022-01-20 16:04:45 +0900

    Initial Commit

---
+++ TestApp4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp4
+//
+//  Created by je-kim on 2022/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
